f68192a24f237b95cc8a7ebba6be588517a862e9
avril-m-harder <avrilharder@gmail.com>
2023-08-04 10:58:29 -0500

    update

--- simulated/bash/98_bcftoolsroh_hmm_tests/bcftoolsROH_HMM_tests.sh
+++ simulated/bash/98_bcftoolsroh_hmm_tests/bcftoolsROH_HMM_tests.sh
@@ -42,6 +42,7 @@ mkdir output
 declare -a cvgX=(05x 10x 15x 30x)
 declare -a hw2az=(2.00e-6 6.7e-5 6.7e-6 6.7e-7 6.7e-8 6.7e-9 6.7e-10 6.7e-11)
 declare -a az2hw=(1.16e-5 5e-4 5e-5 5e-6 5e-7 5e-8 5e-9 5e-10 5e-11 5e-12)
+declare -a vit=(1e-10 1e-12)
 
 # -----------------------------------------------------------------------------
 # Generate allele frequency files for use with bcftools roh & index with tabix
@@ -66,61 +67,94 @@ declare -a az2hw=(1.16e-5 5e-4 5e-5 5e-6 5e-7 5e-8 5e-9 5e-10 5e-11 5e-12)
 # -----------------------------------------------------------------------------
 for c in ${cvgX[@]}; do
 	
-#	## Try out Viterbi training option with single value
-#	## Genotypes
-# 	bcftools roh \
-# 		--GTs-only 30 \
-# 		--threads 20 \
-# 		-V 1e-10 \
-# 		-o ./output/GT_sample_pop_100_cvg_${c}_viterbi.txt \
-# 		--AF-file ./af_files/sample_pop_100_cvg_${c}_filtered_SNPs.tab.gz \
-# 		./data/sample_pop_100_cvg_${c}_filtered_SNPs.vcf.gz
-# 
-# 	grep "^VT" ./output/GT_sample_pop_100_cvg_${c}_viterbi.txt >\
-# 	./vt_files/GT_sample_pop_100_cvg_${c}_VT_ONLY.txt
-# 		
-# 
-# 	## Genotype likelihoods
-# 	bcftools roh \
-# 		--threads 20 \
-# 		-V 1e-10 \
-# 		-o ./output/PL_sample_pop_100_cvg_${c}_viterbi.txt \
-# 		--AF-file ./af_files/sample_pop_100_cvg_${c}_filtered_SNPs.tab.gz \
-# 		./data/sample_pop_100_cvg_${c}_filtered_SNPs.vcf.gz
-# 
-# 	grep "^VT" ./output/PL_sample_pop_100_cvg_${c}_viterbi.txt >\
-# 	./vt_files/PL_sample_pop_100_cvg_${c}_VT_ONLY.txt
+	# -------------------------------------------------------------------------
+	# Straight defaults
+	# -------------------------------------------------------------------------
+	## Genotypes
+	bcftools roh \
+		--GTs-only 30 \
+		--threads 20 \
+		-o ./output/GT_sample_pop_100_cvg_${c}_defaults.txt \
+		--AF-file ./af_files/sample_pop_100_cvg_${c}_filtered_SNPs.tab.gz \
+		./data/sample_pop_100_cvg_${c}_filtered_SNPs.vcf.gz
 		
+	grep "^RG" ./output/GT_sample_pop_100_cvg_${c}_defaults.txt >\
+	./rg_files/GT_sample_pop_100_cvg_${c}_defaults_RG_ONLY.txt
 
-	## Test out combinations of transition probabilities
-	for a in ${hw2az[@]}; do
-		for h in ${az2hw[@]}; do
-			
-			## Genotypes
-			bcftools roh \
-				--GTs-only 30 \
-				--threads 20 \
-				-a ${a} \
-				-H ${h} \
-				-o ./output/GT_sample_pop_100_cvg_${c}_hw2az_${a}_az2hw_${h}.txt \
-				--AF-file ./af_files/sample_pop_100_cvg_${c}_filtered_SNPs.tab.gz \
-				./data/sample_pop_100_cvg_${c}_filtered_SNPs.vcf.gz
-				
-			grep "^RG" ./output/GT_sample_pop_100_cvg_${c}_hw2az_${a}_az2hw_${h}.txt >\
-			./rg_files/GT_sample_pop_100_cvg_${c}_hw2az_${a}_az2hw_${h}_RG_ONLY.txt
+	## Genotype likelihoods
+	bcftools roh \
+		--threads 20 \
+		-o ./output/PL_sample_pop_100_cvg_${c}_defaults.txt \
+		--AF-file ./af_files/sample_pop_100_cvg_${c}_filtered_SNPs.tab.gz \
+		./data/sample_pop_100_cvg_${c}_filtered_SNPs.vcf.gz
+		
+	grep "^RG" ./output/PL_sample_pop_100_cvg_${c}_defaults.txt >\
+	./rg_files/PL_sample_pop_100_cvg_${c}_defaults_RG_ONLY.txt
 
-			## Genotype likelihoods
-			bcftools roh \
-				--threads 20 \
-				-a ${a} \
-				-H ${h} \
-				-o ./output/PL_sample_pop_100_cvg_${c}_hw2az_${a}_az2hw_${h}.txt \
-				--AF-file ./af_files/sample_pop_100_cvg_${c}_filtered_SNPs.tab.gz \
-				./data/sample_pop_100_cvg_${c}_filtered_SNPs.vcf.gz
-				
-			grep "^RG" ./output/PL_sample_pop_100_cvg_${c}_hw2az_${a}_az2hw_${h}.txt >\
-			./rg_files/PL_sample_pop_100_cvg_${c}_hw2az_${a}_az2hw_${h}_RG_ONLY.txt
-				
-		done
-	done	
+	# -------------------------------------------------------------------------
+	# Testing Viterbi training + options
+	# -------------------------------------------------------------------------
+
+# 	for v in ${vit[@]}; do
+# 	
+# 		## Genotypes
+# 		bcftools roh \
+# 			--GTs-only 30 \
+# 			--threads 20 \
+# 			-V ${v} \
+# 			-o ./output/GT_sample_pop_100_cvg_${c}_viterbi_${v}.txt \
+# 			--AF-file ./af_files/sample_pop_100_cvg_${c}_filtered_SNPs.tab.gz \
+# 			./data/sample_pop_100_cvg_${c}_filtered_SNPs.vcf.gz
+# 	
+# 		grep "^VT" ./output/GT_sample_pop_100_cvg_${c}_viterbi_${v}.txt >\
+# 		./vt_files/GT_sample_pop_100_cvg_${c}_${v}_VT_ONLY.txt
+# 			
+# 	
+# 		## Genotype likelihoods
+# 		bcftools roh \
+# 			--threads 20 \
+# 			-V ${v} \
+# 			-o ./output/PL_sample_pop_100_cvg_${c}_viterbi_${v}.txt \
+# 			--AF-file ./af_files/sample_pop_100_cvg_${c}_filtered_SNPs.tab.gz \
+# 			./data/sample_pop_100_cvg_${c}_filtered_SNPs.vcf.gz
+# 	
+# 		grep "^VT" ./output/PL_sample_pop_100_cvg_${c}_viterbi_${v}.txt >\
+# 		./vt_files/PL_sample_pop_100_cvg_${c}_${v}_VT_ONLY.txt
+# 	
+# 	done
+
+	# -------------------------------------------------------------------------
+	# Testing combinations of specified HMM transition probabilities
+	# -------------------------------------------------------------------------
+
+# 	for a in ${hw2az[@]}; do
+# 		for h in ${az2hw[@]}; do
+# 			
+# 			## Genotypes
+# 			bcftools roh \
+# 				--GTs-only 30 \
+# 				--threads 20 \
+# 				-a ${a} \
+# 				-H ${h} \
+# 				-o ./output/GT_sample_pop_100_cvg_${c}_hw2az_${a}_az2hw_${h}.txt \
+# 				--AF-file ./af_files/sample_pop_100_cvg_${c}_filtered_SNPs.tab.gz \
+# 				./data/sample_pop_100_cvg_${c}_filtered_SNPs.vcf.gz
+# 				
+# 			grep "^RG" ./output/GT_sample_pop_100_cvg_${c}_hw2az_${a}_az2hw_${h}.txt >\
+# 			./rg_files/GT_sample_pop_100_cvg_${c}_hw2az_${a}_az2hw_${h}_RG_ONLY.txt
+# 
+# 			## Genotype likelihoods
+# 			bcftools roh \
+# 				--threads 20 \
+# 				-a ${a} \
+# 				-H ${h} \
+# 				-o ./output/PL_sample_pop_100_cvg_${c}_hw2az_${a}_az2hw_${h}.txt \
+# 				--AF-file ./af_files/sample_pop_100_cvg_${c}_filtered_SNPs.tab.gz \
+# 				./data/sample_pop_100_cvg_${c}_filtered_SNPs.vcf.gz
+# 				
+# 			grep "^RG" ./output/PL_sample_pop_100_cvg_${c}_hw2az_${a}_az2hw_${h}.txt >\
+# 			./rg_files/PL_sample_pop_100_cvg_${c}_hw2az_${a}_az2hw_${h}_RG_ONLY.txt
+# 				
+# 		done
+# 	done	
 done
