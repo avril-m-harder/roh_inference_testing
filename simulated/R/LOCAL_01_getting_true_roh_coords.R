417dd97b388013501e075cf2a5d03b4da729f255
avril-m-harder <avrilharder@gmail.com>
2023-08-15 17:01:44 -0500

    updating local R scripts

--- simulated/R/LOCAL_01_getting_true_roh_coords.R
+++ simulated/R/LOCAL_01_getting_true_roh_coords.R
@@ -1,4 +1,4 @@
-setwd('/Users/Avril/Desktop/roh_param_project/simulated_plotting_summarizing/bcftoolsroh_output/')
+setwd('/Users/Avril/Documents/roh_param_project/roh_inference_testing/simulated/data/slim_true_data/')
 library(ape)
 library(scales)
 `%notin%` <- Negate(`%in%`)
@@ -7,85 +7,78 @@ library(scales)
 ## set chromosome length for SLiM run
 c.len <- 30e6
 
-## read in ancestral fasta:
-## homozygous, so read in twice.
-## bases not included in VCF assumed to still be homozygous.
-anc.1 <- read.FASTA('/Users/Avril/Desktop/roh_param_project/SLiM/22_01_06_testing_slim/ancestral.fasta')
-anc.1 <- as.character(anc.1)
-
+fns <- list.files()
+## for each demograhpic scenario,
 ## read in VCF
-vcf <- read.table('/Users/Avril/Desktop/roh_param_project/SLiM/22_01_06_testing_slim/final_pop.vcf', header=TRUE)
-head(vcf[,c(1:10)])
-
-## convert reference alleles in VCF to nuc at that position in ancestral fasta
-bases <- c('A','T','C','G')
-for(i in 1:nrow(vcf)){
-  ref <- toupper(anc.1$`1`[vcf$POS[i]])
-  vcf[i, 'REF'] <- ref
-  vcf[i, 'ALT'] <- sample(bases[bases %notin% ref], 1)
-}
-all(vcf$ALT != vcf$REF) ## verify that all REF and ALT alleles differ for each locus (should be TRUE)
-
-##### Check ROH / heterozygosity statistics #####
-## check ROH length distributions based on variants in VCF
-samp.cols <- grep('i', colnames(vcf)) ## get columns that contain sample data
-
-OUT <- NULL    ## where to save het sites for each individual
-# OUT1 <- NULL   ## where to save true ROH coordinates for each individual
-for(col in samp.cols){
-  print(colnames(vcf)[col])
-  samp <- gsub('i','',colnames(vcf)[col])
-  temp <- vcf[,c(2,col)] ## subset to sample genotypes
-  temp[temp[,2] == '0|1', 2] <- '1'
-  temp[temp[,2] == '1|0', 2] <- '1'
-  temp[temp[,2] == '1|1', 2] <- '0'
-  temp[temp[,2] == '0|0', 2] <- '0'
-  het.sites <- temp[temp[,2] == 1,]
-  het.sites <- het.sites[order(het.sites$POS),]
-  het.sites[,2] <- samp
-  colnames(het.sites) <- c('het.pos','samp')
-  OUT <- rbind(OUT, het.sites)
+for(f in fns){
+  vcf <- read.table(f, header=TRUE)
+  demo <- strsplit(f, split = '_')[[1]][2]
+  head(vcf[,c(1:10)])
   
-  # for(h in 1:nrow(het.sites)){
-  #   if(h < nrow(het.sites) & het.sites[h, 1] == (het.sites[h+1, 1] + 1)){
-  #     print(paste0(colnames(vcf)[col]," - CONSECUTIVE HET SITES"))
-  #   } else{
-  #     if(h == 1){
-  #       s <- 1
-  #       e <- het.sites[h, 1] - 1
-  #       save <- c(samp, s, e, (e-s+1))
-  #       OUT1 <- rbind(OUT1, save)
-  #     } 
-  #     if(h == nrow(het.sites)){
-  #       s <- het.sites[h-1, 1] + 1
-  #       e <- het.sites[h, 1] - 1
-  #       save <- c(samp, s, e, (e-s+1))
-  #       OUT1 <- rbind(OUT1, save)
-  #       s <- het.sites[h, 1] + 1
-  #       e <- c.len
-  #       save <- c(samp, s, e, (e-s+1))
-  #       OUT1 <- rbind(OUT1, save)
-  #     } 
-  #     if(h != 1 & h != nrow(het.sites)){
-  #       s <- het.sites[h-1, 1] + 1
-  #       e <- het.sites[h, 1] - 1
-  #       save <- c(samp, s, e, (e-s+1))
-  #       OUT1 <- rbind(OUT1, save)
-  #     }
-  #   }
-  # }
-  if(col == min(samp.cols)){
-    write.table(OUT, '../true_indiv_data/true_het_sites.txt', sep = '\t', row.names = FALSE, quote = FALSE, col.names = FALSE)
-    # write.table(OUT1, '../true_indiv_data/true_roh_coords.txt', sep = '\t', row.names = FALSE, quote = FALSE, col.names = FALSE)
-    OUT <- NULL
-    # OUT1 <- NULL
-  } else{
-    write.table(OUT, '../true_indiv_data/true_het_sites.txt', sep = '\t', row.names = FALSE, quote = FALSE, col.names = FALSE, append = TRUE)
-    # write.table(OUT1, '../true_indiv_data/true_roh_coords.txt', sep = '\t', row.names = FALSE, quote = FALSE, col.names = FALSE, append = TRUE)
-    OUT <- NULL
-    # OUT1 <- NULL
+  ##### Check ROH / heterozygosity statistics #####
+  ## check ROH length distributions based on variants in VCF
+  samp.cols <- grep('i', colnames(vcf)) ## get columns that contain sample data
+  
+  OUT <- NULL    ## where to save het sites for each individual
+  OUT1 <- NULL   ## where to save true ROH coordinates for each individual
+  for(col in samp.cols){
+    print(colnames(vcf)[col])
+    samp <- gsub('i','',colnames(vcf)[col])
+    temp <- vcf[,c(2,col)] ## subset to sample genotypes
+    temp[temp[,2] == '0|1', 2] <- '1'
+    temp[temp[,2] == '1|0', 2] <- '1'
+    temp[temp[,2] == '1|1', 2] <- '0'
+    temp[temp[,2] == '0|0', 2] <- '0'
+    het.sites <- temp[temp[,2] == 1,]
+    het.sites <- het.sites[order(het.sites$POS),]
+    het.sites[,2] <- samp
+    colnames(het.sites) <- c('het.pos','samp')
+    OUT <- rbind(OUT, het.sites)
+    
+    for(h in 1:nrow(het.sites)){
+      if(h < nrow(het.sites) & het.sites[h, 1] == (het.sites[h+1, 1] + 1)){
+        print(paste0(colnames(vcf)[col]," - CONSECUTIVE HET SITES"))
+      } else{
+        if(h == 1){
+          s <- 1
+          e <- het.sites[h, 1] - 1
+          save <- c(samp, s, e, (e-s+1))
+          OUT1 <- rbind(OUT1, save)
+        }
+        if(h == nrow(het.sites)){
+          s <- het.sites[h-1, 1] + 1
+          e <- het.sites[h, 1] - 1
+          save <- c(samp, s, e, (e-s+1))
+          OUT1 <- rbind(OUT1, save)
+          s <- het.sites[h, 1] + 1
+          e <- c.len
+          save <- c(samp, s, e, (e-s+1))
+          OUT1 <- rbind(OUT1, save)
+        }
+        if(h != 1 & h != nrow(het.sites)){
+          s <- het.sites[h-1, 1] + 1
+          e <- het.sites[h, 1] - 1
+          save <- c(samp, s, e, (e-s+1))
+          OUT1 <- rbind(OUT1, save)
+        }
+      }
+    }
+    if(col == min(samp.cols)){
+      write.table(OUT, paste0(demo,'_true_het_sites.txt'), sep = '\t', row.names = FALSE, 
+                  quote = FALSE, col.names = FALSE)
+      write.table(OUT1, paste0(demo,'_true_roh_coords.txt'), sep = '\t', row.names = FALSE, 
+                  quote = FALSE, col.names = FALSE)
+      OUT <- NULL
+      OUT1 <- NULL
+    } else{
+      write.table(OUT, paste0(demo,'_true_het_sites.txt'), sep = '\t', row.names = FALSE, 
+                  quote = FALSE, col.names = FALSE, append = TRUE)
+      write.table(OUT1, paste0(demo,'_true_roh_coords.txt'), sep = '\t', row.names = FALSE, 
+                  quote = FALSE, col.names = FALSE, append = TRUE)
+      OUT <- NULL
+      OUT1 <- NULL
+    }
   }
 }
-
 
 
--- simulated/R/LOCAL_02a_summarize_bcftoolsROH_output.R
+++ simulated/R/LOCAL_02a_summarize_bcftoolsROH_output.R
@@ -4,68 +4,37 @@
 
 library(scales)
 
-setwd('/Users/Avril/Desktop/roh_param_project/simulated_plotting_summarizing/bcftoolsroh_output/')
+setwd('/Users/Avril/Documents/roh_param_project/roh_inference_testing/simulated/data/bcftools_output/')
 
 ## get filenames of GT-only and PL results
 fns <- list.files()
-gt.fns <- fns[grep('_gt_', fns)]
-pl.fns <- fns[grep('_pl_', fns)]
 
-##### 1. Read in bcftools/ROH results, summarize, and write files with summary stats and ROH coordinates (can read in at #3) #####
+##### Read in bcftools/ROH results, summarize, and write files with summary stats and ROH coordinates #####
 ## loop over GT-only results to summarize ROH call data
-OUT <- NULL ## will store coordinates of all identified ROHs for all pop size / covg / sample
-for(f in gt.fns){
+OUT <- NULL ## will store coordinates of all identified ROHs for all method / demo scenario / covg / sample
+for(f in fns){
   print(f)
-  splt.f <- do.call(rbind, strsplit(f, split='_'))
-  ## get pop size and coverage from filename
-  pop.size <- splt.f[1,3]
-  covg <- gsub('x', '', splt.f[1,5])
+  comps <- unlist(strsplit(f, split = '_'))
+  demo <- comps[1]
+  method <- comps[2]
+  covg <- comps[3]
+  hmm <- comps[4]
   ## read in data
   dat <- read.table(f)
   dat <- dat[,-1]
   colnames(dat) <- c('id','chrom','start','end','length','num.snps','quality')
-  ## check for match here
-  print(paste0('pop size = ',pop.size,' - number of samples = ',length(unique(dat$id))))
+  ## summarize and save data
   for(i in unique(dat$id)){
     sub <- dat[dat$id == i,]
     ## save coordinates of all identified ROHs in every sample
     sub3 <- sub[,c(1,3:7)]
-    sub3$pop.size <- pop.size
+    sub3$demo <- demo
+    sub3$method <- method
     sub3$covg <- covg
-    sub3$id <- gsub('i','',sub3$id)
-    sub3 <- sub3[,c(1,7,8,2:6)]
+    sub3$hmm <- hmm
+    sub3 <- sub3[,c(1,7:10,2:6)]
     OUT <- rbind(OUT, sub3)
   }
 }
-## write results
-write.table(OUT, '/Users/Avril/Desktop/roh_param_project/simulated_plotting_summarizing/bcftoolsroh_results/bcftoolsROH_GT_all_coordinates.txt', 
+write.table(OUT, 'bcftoolsROH_all_coordinates.txt', 
             row.names=FALSE, quote = FALSE, sep = '\t')
-
-## loop over PL results to summarize ROH call data
-OUT <- NULL ## will store coordinates of all identified ROHs for all pop size / covg / sample
-for(f in pl.fns){
-  print(f)
-  splt.f <- do.call(rbind, strsplit(f, split='_'))
-  ## get pop size and coverage from filename
-  pop.size <- splt.f[1,3]
-  covg <- gsub('x', '', splt.f[1,5])
-  ## read in data
-  dat <- read.table(f)
-  dat <- dat[,-1]
-  colnames(dat) <- c('id','chrom','start','end','length','num.snps','quality')
-  ## check for match here
-  print(paste0('pop size = ',pop.size,' - number of samples = ',length(unique(dat$id))))
-  for(i in unique(dat$id)){
-    sub <- dat[dat$id == i,]
-    ## save coordinates of all identified ROHs in every sample
-    sub3 <- sub[,c(1,3:7)]
-    sub3$pop.size <- pop.size
-    sub3$covg <- covg
-    sub3$id <- gsub('i','',sub3$id)
-    sub3 <- sub3[,c(1,7,8,2:6)]
-    OUT <- rbind(OUT, sub3)
-  }
-}
-## write results
-write.table(OUT, '/Users/Avril/Desktop/roh_param_project/simulated_plotting_summarizing/bcftoolsroh_results/bcftoolsROH_PL_all_coordinates.txt', 
-          row.names=FALSE, quote = FALSE, sep = '\t')
